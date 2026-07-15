9ce6352684dba2745b5e61f36d70cc8e77aad026
NexoCFO <jpj74dtvgm@privaterelay.appleid.com>
2026-07-14 22:17:56 -0300

    release 0.3.1 - auth module

---
+++ app/features/auth/components/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "../components/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <main>
+      <h1>NEXO Login</h1>
+
+      <LoginForm />
+    </main>
+  );
+}
